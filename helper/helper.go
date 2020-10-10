cea8dd90d833ed20c5e328059de2510375ba2e17
amtoaer <amtoaer@outlook.com>
2020-10-10 10:00:17 +0800

    refactor: 使用readByteBody代替readBody，方便使用

--- helper/helper.go
+++ helper/helper.go
@@ -6,14 +6,11 @@ import (
 	"net/http"
 )
 
-// ReadBody 读取response内容并关闭流
-func ReadBody(r *http.Response) (string, error) {
+// ReadByteBody 读取response内容并关闭流（返回[]byte结果）
+func ReadByteBody(r *http.Response) ([]byte, error) {
 	content, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
-	if err != nil {
-		return "", err
-	}
-	return string(content), nil
+	return content, err
 }
 
 // WriteSuccessHTML 为本地web服务器显示授权成功界面
