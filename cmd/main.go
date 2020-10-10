fb1e158a96944bd58f1ae75fc4920e923f1ca228
amtoaer <amtoaer@outlook.com>
2020-10-10 15:24:01 +0800

    test: 在main.go中对需要登陆的api.UserProgress进行测试

--- cmd/main.go
+++ cmd/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"bangumi/internal/login"
+	"encoding/json"
 	"fmt"
 )
 
 func main() {
 	api := login.Login("bgm16655f7fd3916a663", "47b62b4cf43cc36716753e83eb13725a", "5212").NewSession()
-	result, err := api.UserInfo("amtoaer")
-	if err != nil {
-		return
-	}
-	fmt.Println(result)
+	result, _ := api.UserProgress("amtoaer", "")
+	content, _ := json.Marshal(result)
+	fmt.Println(string(content))
 }
