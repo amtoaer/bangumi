ff3f682873c63ba0101499423f03723fbe7323a5
amtoaer <amtoaer@outlook.com>
2020-10-10 10:04:08 +0800

    test: 修改main函数，测试UserInfo功能

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "bangumi/internal/login"
+import (
+	"bangumi/internal/login"
+	"fmt"
+)
 
 func main() {
-	login.Login("bgm16655f7fd3916a663", "47b62b4cf43cc36716753e83eb13725a", "5212")
+	api := login.Login("bgm16655f7fd3916a663", "47b62b4cf43cc36716753e83eb13725a", "5212").NewSession()
+	result, err := api.UserInfo("amtoaer")
+	if err != nil {
+		return
+	}
+	fmt.Println(result)
 }
